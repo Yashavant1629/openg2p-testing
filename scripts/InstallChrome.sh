9cd6cdcc7fb4589c86670646ae216f00380edbb5
Yashavant1629 <yashavant893@gmail.com>
2023-12-21 12:28:18 +0530

    added commands to install chromedriver

--- scripts/InstallChrome.sh
+++ scripts/InstallChrome.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -ex
 
-CHROME_VERSION="114.0.5735.90"
+CHROME_VERSION="114"
+CHROMEDRIVER_VERSION="114.0.5735.90"
 
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_${CHROME_VERSION}_amd64.deb
 sudo apt install ./google-chrome-stable_${CHROME_VERSION}_amd64.deb
+
+
+wget -N https://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip -P /tmp/
+sudo unzip /tmp/chromedriver_linux64.zip -d /usr/local/bin/
+sudo chmod +x /usr/local/bin/chromedriver
